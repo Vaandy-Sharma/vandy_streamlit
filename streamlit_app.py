7928162d59510817f687ef0367ccd0228204ac82
Vandy <109974710+Vaandy-Sharma@users.noreply.github.com>
2024-06-24 15:17:23 +0000

    Initial Commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,119 +1,77 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+import requests
+import matplotlib.pyplot as plt
+url = f'https://pokeapi.co/api/v2/pokemon/5/'
+response = requests.get(url)
+pokemon = response.json()
+name = pokemon['name']
+height = pokemon['height']
+weight = pokemon['weight']
+
+url = f'https://pokeapi.co/api/v2/pokemon/6/'
+response = requests.get(url)
+pokemon2 = response.json()
+name2 = pokemon2['name']
+height2 = pokemon2['height']
+weight2 = pokemon2['weight']
+
+url = f'https://pokeapi.co/api/v2/pokemon/7/'
+response = requests.get(url)
+pokemon3 = response.json()
+name3 = pokemon3['name']
+height3 = pokemon3['height']
+weight3 = pokemon3['weight']
 
 
-st.title("📊 Data evaluation app")
 
-st.write(
-    "We are so glad to see you here. ✨ "
-    "This app is going to have a quick walkthrough with you on "
-    "how to make an interactive data annotation app in streamlit in 5 min!"
-)
+name = pokemon['name']
+st.title(name.capitalize())
 
-st.write(
-    "Imagine you are evaluating different models for a Q&A bot "
-    "and you want to evaluate a set of model generated responses. "
-    "You have collected some user data. "
-    "Here is a sample question and response set."
-)
-
-data = {
-    "Questions": [
-        "Who invented the internet?",
-        "What causes the Northern Lights?",
-        "Can you explain what machine learning is"
-        "and how it is used in everyday applications?",
-        "How do penguins fly?",
-    ],
-    "Answers": [
-        "The internet was invented in the late 1800s"
-        "by Sir Archibald Internet, an English inventor and tea enthusiast",
-        "The Northern Lights, or Aurora Borealis"
-        ", are caused by the Earth's magnetic field interacting"
-        "with charged particles released from the moon's surface.",
-        "Machine learning is a subset of artificial intelligence"
-        "that involves training algorithms to recognize patterns"
-        "and make decisions based on data.",
-        " Penguins are unique among birds because they can fly underwater. "
-        "Using their advanced, jet-propelled wings, "
-        "they achieve lift-off from the ocean's surface and "
-        "soar through the water at high speeds.",
-    ],
-}
-
-df = pd.DataFrame(data)
-
-st.write(df)
-
-st.write(
-    "Now I want to evaluate the responses from my model. "
-    "One way to achieve this is to use the very powerful `st.data_editor` feature. "
-    "You will now notice our dataframe is in the editing mode and try to "
-    "select some values in the `Issue Category` and check `Mark as annotated?` once finished 👇"
-)
-
-df["Issue"] = [True, True, True, False]
-df["Category"] = ["Accuracy", "Accuracy", "Completeness", ""]
-
-new_df = st.data_editor(
-    df,
-    column_config={
-        "Questions": st.column_config.TextColumn(width="medium", disabled=True),
-        "Answers": st.column_config.TextColumn(width="medium", disabled=True),
-        "Issue": st.column_config.CheckboxColumn("Mark as annotated?", default=False),
-        "Category": st.column_config.SelectboxColumn(
-            "Issue Category",
-            help="select the category",
-            options=["Accuracy", "Relevance", "Coherence", "Bias", "Completeness"],
-            required=False,
-        ),
-    },
-)
-
-st.write(
-    "You will notice that we changed our dataframe and added new data. "
-    "Now it is time to visualize what we have annotated!"
-)
-
-st.divider()
-
-st.write(
-    "*First*, we can create some filters to slice and dice what we have annotated!"
-)
-
-col1, col2 = st.columns([1, 1])
+col1, col2 = st.columns(2)
 with col1:
-    issue_filter = st.selectbox("Issues or Non-issues", options=new_df.Issue.unique())
+    sprites = pokemon['sprites']
+    picture = sprites.get("front_default")
+    st.image(picture, width = 300)
 with col2:
-    category_filter = st.selectbox(
-        "Choose a category",
-        options=new_df[new_df["Issue"] == issue_filter].Category.unique(),
-    )
+    st.write(f"**Height** - {pokemon['height']}")
+    st.write(f"**Weight** - {pokemon['weight']}")
+    st.write(f"**Base Experience** - {pokemon['base_experience']}")
+    abilities = [ability['ability']['name'] for ability in pokemon['abilities']]
+    st.write("**Abilities**:")
+    for ability in abilities:
+        st.write(f"- {ability.capitalize()}")
+    
 
-st.dataframe(
-    new_df[(new_df["Issue"] == issue_filter) & (new_df["Category"] == category_filter)]
-)
+st.subheader('Cries')
+cries_url = 'https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/5.ogg'
+st.audio(cries_url)
 
-st.markdown("")
-st.write(
-    "*Next*, we can visualize our data quickly using `st.metrics` and `st.bar_plot`"
-)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
 
-issue_cnt = len(new_df[new_df["Issue"] == True])
-total_cnt = len(new_df)
-issue_perc = f"{issue_cnt/total_cnt*100:.0f}%"
 
-col1, col2 = st.columns([1, 1])
-with col1:
-    st.metric("Number of responses", issue_cnt)
-with col2:
-    st.metric("Annotation Progress", issue_perc)
+ax1.bar([name.capitalize(), name2.capitalize(), name3.capitalize()], [height, height2, height3], color=['grey', 'darkkhaki', 'darksalmon'])
+ax1.set_xlabel('Pokémon')
+ax1.set_ylabel('Height')
 
-df_plot = new_df[new_df["Category"] != ""].Category.value_counts().reset_index()
 
-st.bar_chart(df_plot, x="Category", y="count")
 
-st.write(
-    "Here we are at the end of getting started with streamlit! Happy Streamlit-ing! :balloon:"
-)
+ax2.bar([name.capitalize(), name2.capitalize(), name3.capitalize()], [weight, weight2, weight3], color=['grey', 'darkkhaki', 'darksalmon'])
+ax2.set_xlabel('Pokémon')
+ax2.set_ylabel('Weight')
+ax2.set_title('Weight')
+
+st.pyplot(fig)
+st.write('Height and Wight Comparison')
+
+
+
+
+
+
+
+
+
+
 
